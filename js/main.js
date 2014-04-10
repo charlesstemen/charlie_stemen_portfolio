4f58b936fe364ac205709e3786a37befa5d8bb98
Zachary Lincoln <zach@bluehousegroup.com>
2014-04-10 10:46:10 -0400

    added routes and split for multiview

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-var app = angular.module('groceryList', ['ngRoute', 'app.controllers']);
+var app = angular.module('portfolio', ['ngRoute', 'app.controllers']);
 
 var controllers = angular.module('app.controllers', []);
 // var services = angular.module('app.services', []);
 // var directives = angular.module('app.directives', []);
 
 app.run(['$rootScope', '$route', function($rootScope, $route){
-	$rootScope.pageTitle = 'Home | Grocery List';
+	$rootScope.pageTitle = 'Home | Charles Stemen';
 
 	$rootScope.$on('$routeChangeSuccess', function(){
-		$rootScope.pageTitle = $route.current.pageTitle + ' | Grocery List';
+		$rootScope.pageTitle = $route.current.pageTitle + ' | Charles Stemen';
 	})
 }]);
 
@@ -17,6 +17,16 @@ app.config(['$routeProvider', function($routeProvider){
 		templateUrl: 'partials/layout/home.html',
 		controller: 'HomeCtrl',
 		pageTitle: 'Home'
+	})
+	.when('/show/:category', {
+		templateUrl: 'partials/layout/home.html',
+		controller: 'HomeCtrl',
+		pageTitle: ':category'
+	})
+	.when('/id/:projectId', {
+		templateUrl: 'partials/layout/detail.html',
+		controller: 'DetailCtrl',
+		pageTitle: ':projectId'
 	})
 	.when('/error', {
 		templateUrl: 'partials/layout/error.html',
