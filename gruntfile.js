2e8a03115fa27953de21bc4af5cb4736f82b3eae
zlincoln <znlincoln@gmail.com>
2015-03-21 23:16:27 -0400

    final touches

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,34 @@
 module.exports = function(grunt){
 	grunt.initConfig({
+		less: {
+	      development: {
+	        options: {
+	          compress: true,
+	          yuicompress: true,
+	          optimization: 2,
+	          paths: ["css"],
+	          livereload: true,
+	          sourceMap: true,
+	          sourceMapFilename: 'css/master.css.map',
+	          sourceMapUrl: 'css/master.css.map',
+	          sourceMapRootpath: 'http://localhost:8888/charlie_stemen_portfolio/',
+	        },
+	        files: {
+	          'css/master.css': 'css/master.less'
+	        }
+	      }
+	    },
+	    watch: {
+	      less: {
+	        options: {
+	          livereload: true
+	        },
+	        files: [
+	          'css/**/*.less'
+	        ],
+	        tasks: ['less:development']
+	      }
+	    },
 		htmlmin: {
 			dist: {
 				options: {
@@ -44,6 +73,8 @@ module.exports = function(grunt){
 	// grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('default', ['htmlmin', 'uglify']);
 }
